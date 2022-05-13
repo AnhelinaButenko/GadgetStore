133ca6e3c39d2d36613e5aa325856f07a47d268a
Angelina Butenko <angelinaa2712@gmail.com>
2022-05-13 20:26:11 +0300

    create GadgetStore

--- GadgetStore/NotificationService.cs
+++ GadgetStore/NotificationService.cs
@@ -8,5 +8,28 @@ namespace GadgetStore
 {
     class NotificationService
     {
+        public void Notify(List<Product> products)
+        {
+            foreach (var item in products)
+            {
+                Console.WriteLine($"Id: {item.Id}, Description: {item.Description}, Model: {item.Model}, Price: {item.Price} ");
+            }
+
+            Console.WriteLine("___");
+            var sum = products.Sum(p => p.Price);
+     
+            Console.WriteLine($"Price: {sum}");
+        }
+
+        public int Notify(UserInformation userInformation)
+        {
+            Random rnd = new Random();
+
+            int specialNumber = rnd.Next(50000, 100000);
+
+            Console.WriteLine($"Your order number: {specialNumber} \nYour order will be with you within 3-5 days. Have a good day! =)");
+
+            return specialNumber;
+        }
     }
 }
--- GadgetStore/Program.cs
+++ GadgetStore/Program.cs
@@ -6,7 +6,64 @@ namespace GadgetStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProductService productService = new ProductService();
+
+            var products = productService.GetProducts();
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"Id: {item.Id}, Description: {item.Description}, Model: {item.Model}, Price: {item.Price} ");
+            }
+
+            Console.WriteLine("Input Ids of selected products separated by comma to 10: ");
+
+            var idsString = Console.ReadLine();
+
+            string[] ids = idsString.Split(',');
+            int[] idsNumbers = new int[ids.Length];
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                idsNumbers[i] = Convert.ToInt32(ids[i]);
+            }
+
+            if (idsNumbers.Length > 10)
+            {
+                Console.WriteLine("Input Ids of selected products separated by comma to 10: ");
+                System.Environment.Exit(0);
+            }
+
+            productService.SelectProducts(idsNumbers);
+
+            Console.WriteLine($"Your data: ");
+
+            Console.WriteLine($"Enter name: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Enter last name: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine($"Enter addres: ");
+            var addres = Console.ReadLine();
+
+            Console.WriteLine($"Enter email: ");
+            var email = Console.ReadLine();
+
+            Console.WriteLine($"Enter phone: ");
+            var phone = Console.ReadLine();
+
+            UserInformation userInformation = new UserInformation
+            {
+                Name = name,
+                LastName = lastName,
+                Address = addres,
+                Email = email,
+                Phone = phone
+            };
+
+            NotificationService notificationService = new NotificationService();
+
+            notificationService.Notify(userInformation);
         }
     }
 }
